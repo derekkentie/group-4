4e29fc41a13376514cf067cf8122067739c29ba0
Derek <derek.kentie@gmail.com>
2025-12-04 00:03:22 +0100

    descriptors extraction
    made a function that calculates every descriptor value with rdkit of every molecule in the train and test set.

---
+++ RDkit descriptors extraction function for train and test set.py
@@ -0,0 +1,99 @@
+import numpy as np
+import pandas as pd
+from rdkit import Chem
+from rdkit.Chem import Descriptors
+
+"""
+EXPLANATION OF THE CODE
+
+This function extracts the data from the test- and train.csv files that contain the molecules in SMILES representation,
+after which it uses the rdkit library to calculate 217 molecular descriptors for each molecule.
+
+The descriptors will be filtered down to reduce file size.
+
+At last, the most useful descriptors will be used to train a ML model to predict protein-ligand affinity strength.
+"""
+
+"""
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! WARNING !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+This code generates two excel files in the data file map of the repository with extremely large file sizes, 
+which results in the repository being too large to push to the remote.
+
+The file sizes will be reduced later on when we reduce the descriptors to only the most valuable ones.
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! WARNING !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+"""
+
+"""
+PLANNED IMPROVEMENTS ON THE CODE
+
+    1. The reduction of descriptors based on the follwing factors:
+        (i) removal of descriptors with constant values
+        (ii) removal of descriptors with constant and near-constant variables
+        (iii) removal of descriptors with a standard deviation of less  than 0.001
+        (iv) removal of descriptors with at least one missing value
+        (v) removal of descriptors with a pair correlation larger than or equal to 0.90
+
+    2. The addition of a calculator for the fingerprint presentations of each molecule
+
+    3. Reforming the code into a single function
+
+    4. Applying data controls to make sure the model will not crash
+
+    5. Universalizing the code for more data types
+
+    6. Finding out how to append the descriptor names as header in X
+"""
+
+descriptor_names = [d[0] for d in Descriptors._descList]
+descriptor_functions = [d[1] for d in Descriptors._descList]
+
+#creating starting conditions for variables
+descriptors_per_mol = [] #used to store descriptor values per molecule
+last_progress = -1 #used to keep track on SMILES extraction and descriptor calcuation
+mols = [] #used to store the mol codes per SMILES represented molecule
+X =[] #used to store all descriptor values of every molecule to export to .xlsx file
+
+#specifying which file we will use
+for set in ["train", "test"]:
+    #checking of the sought after file is available
+    try:
+        data = pd.read_csv(f"data/{set}.csv", sep=',')
+    except FileNotFoundError:
+        ("train.csv not found, skipping this file...")
+        data = None
+
+    #extracting the SMILES from the file specific "molecule_SMILES" column into mol code
+    for molecule in range(1, len(data)):       
+        mols.append(Chem.MolFromSmiles(np.array(data)[molecule][list(data).index("molecule_SMILES")])) 
+        #keeping track of the descriptor calculation progress 
+        progress_calculation = int(100*len(mols)/len(data)) 
+        if progress_calculation > last_progress:
+                print(f"progress for {set}set SMILES extraction: {progress_calculation}%")
+                last_progress = progress_calculation
+    last_progress = -1
+
+    #combining every mol with each descriptor calculation function
+    for mol in mols:
+        for func in descriptor_functions:
+            descriptors_per_mol.append(func(mol)) #using each descriptor function on the current mol code to create a list of descriptor values
+        X.append(descriptors_per_mol) #appending the descriptors per molecule as row into X
+        descriptors_per_mol = [] #clearing the descriptor list for to be filled for the next molecule
+
+        #keeping track of the descriptor calculation progress 
+        progress_calculation = int(100*len(X)/len(data)) 
+        if progress_calculation > last_progress:
+            print(f"progress for {set}set descriptor calculation: {progress_calculation}%")
+            last_progress = progress_calculation
+    X = np.array(X)
+    if X.shape[0] == (len(data)-1):
+        print("amount of molecules in X: ", X.shape[0], f" is equal to the amount of molecules in de {set}set: ", (len(data)-1))
+    if X.shape[1] == len(descriptor_functions):
+        print("amount of descriptors in X: ",X.shape[1], " is equal to the amount of descriptors from RDkit: ", len(descriptor_functions))
+    np.savetxt(f"data/{set}_with_all_descriptors.xlsx", X, delimiter=",")
+    #resetting the starting conditions
+    descriptors_per_mol = []
+    last_progress = -1
+    mols = []
+    X =[]
