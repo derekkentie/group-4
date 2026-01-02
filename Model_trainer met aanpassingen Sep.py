c43b3d5342168cbeb9101a10b20aec7456c9c0f7
7628741-596 <s.a.meulders@student.tue.nl>
2026-01-02 15:04:18 +0100

    nieuwe picklefiles

--- Model_trainer met aanpassingen Sep.py
+++ Model_trainer met aanpassingen Sep.py
@@ -37,6 +37,18 @@ class Modeltrainer:
     program, named 'Model_trainer.py', while making use of the gpu's.
     """
 
+# #added by Sep:
+# """This code is added to make it easier to select specific physicochemical properties to use to make the model. 
+# It does so by first creating a dictionary coupling every aminoacid to a subsequent featurevector, after which new dictionaries are made coupling IDs to featurevectors or -matrices,
+# make sure you keep the dictionaries you need for the model. If you want to use a picklefile directly for your model, feel free to use ctrl + / to silence the added code."""
+
+
+# ID_to_protein = pickle.load(open(r"C:\Users\20243625\OneDrive - TU Eindhoven\Desktop\group-4\docs\Sep's picklebestanden\dict ID to sequence",'rb'))
+# aa_to_featurevec_unfilter = pickle.load(open(r"C:\Users\20243625\OneDrive - TU Eindhoven\Desktop\group-4\docs\Sep's picklebestanden\dict aminoacid to property vector 2",'rb')) 
+
+# dict_to_concatenate =None
+# #Above PAM250 is selected, can be exchanged with BLOSUM62, make sure it is a dictionary coupling aminoacids to vectors, not ID's to vectors, can be set to None if you only want to use physicochemical properties
+
     def __init__(self):
         self.dict_of_representation_combination_methods = {
             "feature concatenation": self.feature_concatenation,
@@ -65,11 +77,9 @@ class Modeltrainer:
                 "alpha": [1e-4, 1e-3, 1e-2]
             },
             "Random Forest": {
-                "n_estimators": [10], #200 or 500 will take too long
+                "n_estimators": [200, 500],
                 "max_depth": [None, 20],
-                "min_samples_split": [2, 5],
-                "max_features":[None,'sqrt','log2'],
-                "criterion": ['absolute_error','squared_error']
+                "min_samples_split": [2, 5]
             },
             "Gradient Boosting Regressor": {
                 "n_estimators": [200, 500],
@@ -168,8 +178,118 @@ class Modeltrainer:
         # 1. Load representations
         mol_rep_dict_of_dicts = self.dicts_collector(
             map_location= "docs\mol representatie picklebestanden"
+# features_used = [0, 1, 2, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24] #contains indices of physicochemical properties kept, feel free to experiment by changing what it contains, do not exceed 24 as there are no properties with a higher index
+# #FYI: these are the highly correlating pairs (abs(R) > 0.9): [(3, 2), (5, 2), (5, 3), (17, 15), (21, 2), (21, 3)]
+# aa_to_featurevec_filtered = dict()
+
+# for aa in aa_to_featurevec_unfilter.keys(): #creating dictionary coupling aminoacids to featurevectors
+#     value = aa_to_featurevec_unfilter[aa]
+#     features_kept = []
+#     for feature in features_used:
+#         features_kept.append(value[feature])
+#     if dict_to_concatenate is not None:
+#         features_kept = np.concatenate([features_kept,dict_to_concatenate[aa]])
+#     aa_to_featurevec_filtered[aa] = features_kept
+
+# #creating dictionaries coupling IDs to featurevectors or -matrices   
+# ID_to_featurevec = dict()
+# ID_to_feature_mat = dict()
+# ID_codes = ID_to_protein.keys()
+
+# for ID in ID_codes:
+#     protein = ID_to_protein[ID]
+#     vec = np.array([])
+#     for aa in protein:
+#         vec = np.concatenate((vec,aa_to_featurevec_filtered[aa]))
+
+#     ID_to_featurevec[ID] = vec
+#     ID_to_feature_mat[ID] = vec.reshape((len(vec)//len(aa_to_featurevec_filtered['A']),len(aa_to_featurevec_filtered['A'])))
+
+# #creating dictionaries coupling IDs to featurevectors or -matrices padded with nans
+# lengths = [len(value) for value in ID_to_featurevec.values()]
+# max_length = max(lengths)
+# dict_ID_to_propertyvector_padded_with_nans = dict()
+# dict_ID_to_propertymatrix_padded_with_nans = dict()
+
+
+# for ID in ID_to_featurevec.keys():
+#     value = ID_to_featurevec[ID]
+#     nr_nans = max_length - len(value)
+#     nan_array = (np.array([float('nan')]*nr_nans))
+#     vec = np.concatenate((value,nan_array))
+#     dict_ID_to_propertyvector_padded_with_nans[ID] = vec
+#     dict_ID_to_propertymatrix_padded_with_nans[ID] = vec.reshape((len(vec)//len(aa_to_featurevec_filtered['A']),len(aa_to_featurevec_filtered['A'])))
+
+# print(dict_ID_to_propertymatrix_padded_with_nans['P31749'][20])
+# #End of: added by Sep
+
+#selecting the preferred dicitionaries
+molecule_features_dict_train = pickle.load(open(r"C:\Users\20243625\OneDrive - TU Eindhoven\Desktop\group-4\docs\mol representatie picklebestanden\train_molecule_descriptor_representation.pkl",'rb'))
+molecule_features_dict_test = pickle.load(open(r"C:\Users\20243625\OneDrive - TU Eindhoven\Desktop\group-4\docs\mol representatie picklebestanden\test_molecule_descriptor_representation.pkl",'rb'))
+# protein_features_dict = pickle.load(open(r"docs\Sep's picklebestanden\dict ID to featurevector in one-hot padded with nans", 'rb'))
+
+#generating dictionary for 'local averages' of protein
+#these two lines can be changed to experiment with different features and hyperparameters
+ID_to_mat_old = pickle.load(open(r"C:\Users\20243625\OneDrive - TU Eindhoven\Desktop\group-4\docs\Sep's picklebestanden\dict ID to BLOSUM62 matrix",'rb')) #must be a dictionary coupling ID's to matrices
+nr_pieces = 16 #will probably run into semantic errors if it exceeds 146, as the code is not equiped for that, let me know if you want to try 
+add_length = True
+
+ID_to_protein = pickle.load(open(r"C:\Users\20243625\OneDrive - TU Eindhoven\Desktop\group-4\docs\Sep's picklebestanden\dict ID to sequence",'rb')) 
+nr_features = len(ID_to_mat_old['P24941'][0]) #any featurevector for 1 aa would suffice
+protein_features_dict = dict()
+# ID_to_mat_new = dict()
+
+
+for ID in ID_to_protein.keys():
+    average_length= len(ID_to_protein[ID])/nr_pieces
+    highest_index_processed = -1 #this does not mean the last index
+    residue = 0
+    vec_new = np.array([])
+    for i in range(nr_pieces-1): #-1 because the last iteration is an edge case due to possible floating-point-inaccuracies            
+        length_piece = int(average_length + residue)
+        residue = average_length + residue - int(average_length + residue)
+        aas_in_piece = ID_to_mat_old[ID][range(highest_index_processed+1, highest_index_processed+1+length_piece)]
+        highest_index_processed = highest_index_processed+length_piece
+        
+        average_in_piece = np.average(aas_in_piece,axis=0)
+        vec_new = np.concatenate((vec_new,average_in_piece))
+
+    #last iteration
+    aas_in_piece = ID_to_mat_old[ID][range(highest_index_processed+1,len(ID_to_protein[ID]))]
+    average_in_piece = np.average(aas_in_piece,axis=0)
+    vec_new = np.concatenate((vec_new,average_in_piece))
+
+    #final processing
+    if add_length:
+        protein_features_dict[ID] = np.concatenate((vec_new,np.array([len(ID_to_protein[ID])])))
+        # vec_extended = np.concatenate((vec_new,np.array([len(ID_to_protein[ID])]*nr_features))) #makes sure an entire extra row is added in the matrix containing just the lenghts
+        # ID_to_mat_new[ID] = vec_extended.reshape(len(vec_extended)//nr_features,nr_features)
+        
+    else:
+        protein_features_dict[ID] = vec_new
+        # ID_to_mat_new[ID] = vec_new.reshape(len(vec_new)//nr_features,nr_features)
+print("dictionaries loaded")
+
+
+#loading the training set
+train_df = pd.read_csv("data/train.csv")
+test_df = pd.read_csv("data/test.csv")
+
+X = []
+y = []
+
+#feature concatenation to combining each ligand-protein pair
+
+for _, row in train_df.iterrows():
+    smiles = row["molecule_SMILES"]
+    protein = row["UniProt_ID"]
+    affinity_score = row["affinity_score"]
+    #quick check if all elements are available
+    if smiles not in molecule_features_dict_train: 
+        raise FileNotFoundError(
+            f"The following SMILES exists in the trainingset but not in the molecule-features dictionary: {smiles}"
         )
-        protein_rep_dict_of_dicts = self.dicts_collector(
+        protein_rep_dict_of_dicts = self.dicts_collector( #in the same indented block as the error?
             map_location= "docs/Sep's picklebestanden"
         )
 
@@ -314,6 +434,31 @@ class Modeltrainer:
     def train_test_split(self):
         pass
 ###################################################
+X_predict = []
+for _, row in test_df.iterrows():
+    smiles = row["molecule_SMILES"]
+    protein = row["UniProt_ID"]
+    #quick check if all elements are available
+    if smiles not in molecule_features_dict_test: 
+        raise FileNotFoundError(
+            f"The following SMILES exists in the testset but not in the molecule-features dictionary: {smiles}"
+        )
+    if protein not in protein_features_dict: 
+        raise FileNotFoundError(
+            f"The following Uniprot_ID exists in the testset but not in the protein-features dictionary: {protein}"
+        )
+
+    #feature concatenation
+    if isinstance(molecule_features_dict_test[smiles], np.ndarray):
+        molecule_features_dict_test[smiles] = molecule_features_dict_test[smiles].tolist()
+    if isinstance(protein_features_dict[protein], np.ndarray):
+        protein_features_dict[protein] = protein_features_dict[protein].tolist()
+    combined = molecule_features_dict_test[smiles] + protein_features_dict[protein]
+
+    #data seperation
+    X_predict.append(combined)
+X_predict = np.array(X_predict, dtype=float)
+print("feature concatenation complete")
 
 #splitting the data in training and test sets
 # X_train, X_test, y_train, y_test = train_test_split(
@@ -340,6 +485,12 @@ class Modeltrainer:
 # X = scaler.fit_transform(X)
 # #X_predict = scaler.transform(X_predict)
 # print("minmax scaling complete")
+scaler = MinMaxScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test) #will this make sure the same minimum and maximum is used as in X_train?
+X = scaler.fit_transform(X) #idem
+X_predict = scaler.transform(X_predict)
+print("minmax scaling complete")
 
 #apply PCA if preferred
 r"""
@@ -356,12 +507,12 @@ You can find a list of all estimators that handle NaN values
 at the following page: 
 https://scikit-learn.org/stable/modules/impute.html#estimators-that-handle-nan-values
 """
-"""
-pca = PCA(n_components=8)
-X_train = pca.fit_transform(X_train)
-X_test  = pca.transform(X_test)
-print("PCA application complete")
-"""
+
+# pca = PCA(n_components=8)
+# X_train = pca.fit_transform(X_train)
+# X_test  = pca.transform(X_test)
+# print("PCA application complete")
+
 #NOW APPLY YOUR PREFERRED MODEL TYPE
 r"""
 ValueError: Input X contains NaN.
@@ -378,18 +529,17 @@ at the following page:
 https://scikit-learn.org/stable/modules/impute.html#estimators-that-handle-nan-values
 """
 
-"""
-model = MLPRegressor(
-    hidden_layer_sizes=( 16, 8),
-    activation='logistic',
-    learning_rate='adaptive',
-    max_iter=400,
-    random_state=42
-)
-"""
+# model = MLPRegressor(
+#     hidden_layer_sizes=( 16, 8),
+#     activation='logistic',
+#     learning_rate='adaptive',
+#     max_iter=400,
+#     random_state=42
+# )
+
 
 model = RandomForestRegressor(
-    criterion= 'squared_error', #this is the criterion kaggle uses, but it is slow
+    criterion= 'absolute_error', #this is the criterion kaggle uses, but it is slow
     min_samples_split=2, #considering our large dataset, it might be useful to test bigger values, though based on my runs, it lowers the score significantly.
     min_samples_leaf=1, #idem
     max_leaf_nodes=None, #maybe this is helpful too
@@ -402,6 +552,16 @@ model = RandomForestRegressor(
 # print("Train score:", model.score(X_train, y_train))
 # print("Test score:", model.score(X_test, y_test))
 print(model.__init__)
+
+# model = HistGradientBoostingRegressor(
+#     loss= "absolute_error",
+#     learning_rate= 0.1,
+#     max_iter= 100
+# )
+
+model.fit(X_train, y_train)
+print("Train score:", model.score(X_train, y_train))
+print("Test score:", model.score(X_test, y_test))
 
 #FOR MAKING THE ACTUAL PREDICTIONS
 
---
+++ docs/generating picklefiles for gridsearch.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+import pickle
+import numpy as np
+pathlist = Path(r"C:\Users\20243625\OneDrive - TU Eindhoven\Desktop\group-4\docs\Sep's picklebestanden\protein dicts to use in gridsearch").glob('*')
+
+#these two lines can be changed to experiment with different features and hyperparameters
+ID_to_protein = pickle.load(open(r"C:\Users\20243625\OneDrive - TU Eindhoven\Desktop\group-4\docs\Sep's picklebestanden\dict ID to sequence",'rb')) 
+add_length = True
+nr_pieces_to_test = [5,25,146]
+for path in pathlist:
+    ID_to_mat_old = pickle.load(open(path,'rb')) #must be a dictionary coupling ID's to matrices
+    for nr_pieces  in nr_pieces_to_test: #will probably run into semantic errors if it exceeds 146, as the code is not equiped for that, let me know if you want to try 
+        nr_features = len(ID_to_mat_old['P24941'][0]) #any featurevector for 1 aa would suffice
+        protein_features_dict = dict()
+        # ID_to_mat_new = dict()
+
+
+        for ID in ID_to_protein.keys():
+            average_length= len(ID_to_protein[ID])/nr_pieces
+            highest_index_processed = -1 #this does not mean the last index
+            residue = 0
+            vec_new = np.array([])
+            for i in range(nr_pieces-1): #-1 because the last iteration is an edge case due to possible floating-point-inaccuracies            
+                length_piece = int(average_length + residue)
+                residue = average_length + residue - int(average_length + residue)
+                aas_in_piece = ID_to_mat_old[ID][range(highest_index_processed+1, highest_index_processed+1+length_piece)]
+                highest_index_processed = highest_index_processed+length_piece
+                
+                average_in_piece = np.average(aas_in_piece,axis=0)
+                vec_new = np.concatenate((vec_new,average_in_piece))
+
+            #last iteration
+            aas_in_piece = ID_to_mat_old[ID][range(highest_index_processed+1,len(ID_to_protein[ID]))]
+            average_in_piece = np.average(aas_in_piece,axis=0)
+            vec_new = np.concatenate((vec_new,average_in_piece))
+
+            #final processing
+            if add_length:
+                protein_features_dict[ID] = np.concatenate((vec_new,np.array([len(ID_to_protein[ID])])))
+                # vec_extended = np.concatenate((vec_new,np.array([len(ID_to_protein[ID])]*nr_features))) #makes sure an entire extra row is added in the matrix containing just the lenghts
+                # ID_to_mat_new[ID] = vec_extended.reshape(len(vec_extended)//nr_features,nr_features)
+                
+            else:
+                protein_features_dict[ID] = vec_new
+                # ID_to_mat_new[ID] = vec_new.reshape(len(vec_new)//nr_features,nr_features)
+                
+        pickle.dump(protein_features_dict, open(f"{path} in {nr_pieces} pieces",'wb'))
