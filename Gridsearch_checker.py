19ec850af408120864ad4f833dc34c1bf0d243b5
Derek <derek.kentie@gmail.com>
2026-01-05 15:08:39 +0100

    gridsearch for google colab

--- Gridsearch_checker.py
+++ Gridsearch_checker.py
@@ -1,4 +1,7 @@
 import pandas as pd
 
 gridsearch_csv = pd.read_csv("docs/model_gridsearch.csv")
-print(gridsearch_csv["error"].unique())
+print(gridsearch_csv["error"].unique())gridsearch_csv = pd.read_csv("docs/model_gridsearch_first_run_google_colab_fraction_100.csv")
+successful = gridsearch_csv[gridsearch_csv["error"] == "None"]
+print(len(successful), "successful experiments")
+print(successful.head())
